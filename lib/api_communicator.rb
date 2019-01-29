d4ca8bf731756d8ca46c5ec98d88796f7688b80f
github username <github email address>
2019-01-29 14:30:19 -0500

    added numbers with index

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -16,13 +16,15 @@ def get_characters_data(character_name)
 
   response_hash["results"].find do |character_info|
     character_info["name"].downcase.include? character_name
+
   end
 end
 
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
-  films.each do |film|
-    puts film["title"]
+  films.each.with_index(1) do |film, index|
+    puts "#{index}: #{film["title"]}"
+
   end
 end
 
